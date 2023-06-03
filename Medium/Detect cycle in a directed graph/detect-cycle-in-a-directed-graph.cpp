4a7e7e6a1c7ad1efdbd0aa5675cb7ee880ddc77a
chhawinder <96365487+chhawinder@users.noreply.github.com>
2023-06-03 13:30:35 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
+++ Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
@@ -6,89 +6,33 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in a directed graph.
-    bool dfshelp(int start,int V, vector<int> adj[],vector<int>&vis,vector<int>&path){
-        for(int c:adj[start]){
-            if(vis[c]==-1&&path[c]==0){
-                vis[c]=1;
-                path[c]=1;
-                if(dfshelp(c,V,adj,vis,path)==true)return true;
-            }else if(vis[c]==1&&path[c]==1)return true;
-            
-        }
-        path[start]=0;
-        return false;
-    }
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
-        // vector<int>vis(V,-1),path(V,0);
-        // for(int i=0;i<V;i++){
-        //     if(vis[i]==-1){
-        //         vis[i]=1;
-        //         path[i]=1;
-        //         if(dfshelp(i,V,adj,vis,path)==true)return true;
-        //     }
-        // }
-        // return false;
-        
-        // //using khan's algo
-        
-        // vector<int>ind(V,0);
-        // for(int i=0;i<V;i++){
-        //     for(int it:adj[i]){
-        //         ind[it]++;
-        //     }
-        // }
-        // queue<int>q;
-        // for(int i=0;i<V;i++){
-        //     if(ind[i]==0)q.push(i);
-        // }
-        // vector<int>res;
-        // while(q.size()!=0){
-        //     int t= q.front();
-        //     q.pop();
-        //     res.push_back(t);
-        //     for(int i:adj[t]){
-        //         ind[i]--;
-        //         if(ind[i]==0)q.push(i);
-        //     }
-            
-            
-        // }
-        // if(res.size()!=V)return true;
-        // return false;
-        
-        //toposort
-        vector<int>topo(V,0);
+        vector<int>ind(V,0);
         for(int i=0;i<V;i++){
             for(auto it:adj[i]){
-                topo[it]++;
+                ind[it]++;
             }
         }
-        //queue intiate
         queue<int>q;
         for(int i=0;i<V;i++){
-            
-            if(topo[i]==0)q.push(i);
-        }
-         vector<int>res;
-        while(q.size()!=0){
-            int t=q.front();
-            q.pop();
-            res.push_back(t);
-            for(auto it:adj[t]){
-                topo[it]--;
-                if(topo[it]==0)q.push(it);
+            if(ind[i]==0){
+                q.push(i);
             }
         }
-        if(res.size()==V)return false;
-        return true;
-        
-        
-        
-        
-        
-        
-        
+        while(q.size()!=0){
+            int temp=q.front();q.pop();
+            for(auto it:adj[temp]){
+                ind[it]--;
+                if(ind[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        for(auto it:ind){
+            if(it!=0)return true;
+        }
+        return false;
     }
 };
 
